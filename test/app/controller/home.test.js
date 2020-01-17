5d060f6734f48d9db8fdbcf847c7f476d0f8123e
xiaotao.song <xiaotao.song@molbase.com>
2020-01-17 09:46:59 +0800

    修改ppt

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -5,7 +5,7 @@ const mock = require('egg-mock');
 describe('test/app/controller/home.test.js', () => {
   let app;
   before(() => {
-    // 创建当前应用的 app 实例
+    // 创建当前应用的 app 实例 
     app = mock.app();
     // 等待 app 启动成功，才能执行测试用例
     return app.ready();
